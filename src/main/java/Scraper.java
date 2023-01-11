7954b41adc332e8c3d874ebd8b6f729ecc74e053
ogkourlias <orfeasgkourlias@gmail.com>
2023-01-11 17:17:03 +0100

    Added basic scraper class.

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,2 +1,16 @@
+// Imports
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import net.sourceforge.htmlunit.*;
+
+import java.io.IOException;
+
+// Classes
 public class Scraper {
+
+    public static void clientInit(String url) throws IOException {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        HtmlPage page = webClient.getPage(url);
+    }
 }
